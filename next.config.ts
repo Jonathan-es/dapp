d491ba5963a1c5b9900ebd01189e92b8a9f57585
Jonathan-es <140380609+Jonathan-es@users.noreply.github.com>
2026-01-12 03:04:50 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-output: 'export',
-  images: { unoptimized: true },
-  basePath: '/dapp',
+
 };
 
 export default nextConfig;
